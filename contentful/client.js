f256a20cac4bc9b528d74a2ad9db824398c56e75
vladbalea <vladd.balea@gmail.com>
2023-07-07 16:06:29 +0300

    Posts can be filtered by category now

--- contentful/client.js
+++ contentful/client.js
@@ -7,7 +7,8 @@ const client = createClient({
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 })
 
-export async function getPostsOnPage(page, category = undefined) {
+export async function getPostsOnPage(page, categorySlug = undefined) {
+    const category = categorySlug === undefined ? undefined : await getCategoryBySlug(categorySlug)
     const limit = POSTS_PER_PAGE
     const skip = (page - 1) * limit
 
@@ -16,6 +17,7 @@ export async function getPostsOnPage(page, category = undefined) {
         order: "-sys.createdAt",
         limit,
         skip,
+        ...(category !== undefined && { "fields.category.sys.id": category.id }),
     })
     if (!response.items) {
         return []
@@ -87,10 +89,13 @@ export async function getPostBySlug(slug) {
     }
 }
 
-export async function getNumberOfPages(category = undefined) {
+export async function getNumberOfPages(categorySlug = undefined) {
+    const category = categorySlug === undefined ? undefined : await getCategoryBySlug(categorySlug)
+
     const response = await client.getEntries({
         content_type: "blogPost",
         select: "sys.id",
+        ...(category !== undefined && { "fields.category.sys.id": category.id }),
     })
     if (!response.items) {
         return 0
@@ -98,10 +103,13 @@ export async function getNumberOfPages(category = undefined) {
     return Math.ceil(response.total / POSTS_PER_PAGE)
 }
 
-export async function getPagesNumbersSlugs(category = undefined) {
+export async function getPagesNumbersSlugs(categorySlug = undefined) {
+    const category = categorySlug === undefined ? undefined : await getCategoryBySlug(categorySlug)
+
     const response = await client.getEntries({
         content_type: "blogPost",
         select: "sys.id",
+        ...(category !== undefined && { "fields.category.sys.id": category.id }),
     })
     if (!response.items) {
         return []
@@ -117,4 +125,39 @@ export async function getPagesNumbersSlugs(category = undefined) {
         })
     }
     return paths
+}
+
+export async function getCategoryBySlug(slug) {
+    const response = await client.getEntries({
+        content_type: "category",
+        limit: 1,
+        "fields.slug": slug,
+    })
+    if (!response.items) {
+        console.error(`Could not fetch category: ${slug}!`)
+        return {}
+    }
+    const category = response.items[0]
+
+    return {
+        id: category.sys.id,
+        slug: category.fields.slug,
+        name: category.fields.name,
+    }
+}
+
+export async function getAllCategoriesSlugs() {
+    const response = await client.getEntries({
+        content_type: "category",
+    })
+    if (!response.items) {
+        return []
+    }
+    const categories = response.items
+
+    return categories.map((category) => {
+        return {
+            category: category.fields.slug,
+        }
+    })
 }
