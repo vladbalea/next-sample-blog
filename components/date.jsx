6280cbbaa4949b298763f3cfc2bd710c61eef053
vladbalea <vladd.balea@gmail.com>
2023-07-13 19:09:53 +0300

    Added className prop to Date component

--- components/date.jsx
+++ components/date.jsx
@@ -1,8 +1,11 @@
 import { parseISO, format } from "date-fns"
+import { cn } from "@/lib/utils"
 
-export default function Date({ dateString }) {
+export default function Date({ dateString, className }) {
     const date = parseISO(dateString)
     return (
-        <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>
+        <time dateTime={dateString} className={cn(className && className)}>
+            {format(date, "LLLL d, yyyy")}
+        </time>
     )
 }
