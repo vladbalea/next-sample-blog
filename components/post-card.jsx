a4442cb40cf44b57933b1d2921ff323f8aa3f78b
vladbalea <vladd.balea@gmail.com>
2023-07-13 19:47:35 +0300

    Post card and list styling

--- components/post-card.jsx
+++ components/post-card.jsx
@@ -1,9 +1,30 @@
 import Link from "next/link"
+import Image from "next/image"
+import Date from "./date"
 
 export default function PostCard({ post }) {
     return (
-        <div>
-            <Link href={`/post/${post.slug}`}>{post.title} {post.category && `(${post.category.name})`}</Link>
+        <div className="h-96 md:w-96">
+            <Link href={`/post/${post.slug}`}>
+                <div className="h-full w-full">
+                    {
+                        post.featuredImage &&
+                        <Image
+                            src={post.featuredImage}
+                            width={500}
+                            height={500}
+                            alt={post.title}
+                            className="object-cover h-64 rounded-xl"
+                        />
+                    }
+                    {
+                        post.category &&
+                        <div className="text-sm mt-2 mb-1">{post.category.name.toUpperCase()}</div>
+                    }
+                    <div className="text-lg font-medium mb-1">{post.title}</div>
+                    <Date dateString={post.createdAt} className="text-sm" />
+                </div>
+            </Link>
         </div>
     )
 }
--- components/post-list.jsx
+++ components/post-list.jsx
@@ -6,13 +6,15 @@ export default function PostList({ posts }) {
             {
                 posts.length > 0
                 ?
-                <>
+                <div className="w-full lg:w-[50rem] xl:w-[75rem] mx-auto">
+                    <div className="grid gap-4 justify-items-center lg:grid-cols-2 xl:grid-cols-3">
                     {
                         posts.map((post) => (
                             <PostCard key={post.id} post={post} />
                         ))
                     }
-                </>
+                    </div>
+                </div>
                 :
                 <div>No posts</div>
             }
